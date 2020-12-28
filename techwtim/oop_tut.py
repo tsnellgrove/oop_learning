fbef465abd711ad0063998affdcc021398f1916e
Thomas Snellgrove <tsnellgrove@hotmail.com>
2020-12-28 09:35:45 -0500

    more TwT

--- techwtim/oop_tut.py
+++ techwtim/oop_tut.py
@@ -1,15 +1,41 @@
 # Tech with Tim
+# Python Classes & Objects Tutorial 2
+# Link: https://youtu.be/jQiUOV15IRI
+# Topic:
+
+
+
+
+
+
+
+
 # Python Classes & Objects Tutorial 1
 # Link: https://youtu.be/v_Jp11xqCzg
-# A) 2:20
+# Topic: Objects!
+
+
+import turtle
 
 
 x = 5
 y = 'string'
+f = 5.5
 
-x + 7
+# print(type(x))
+# print(type(y))
 
-print(type(x))
-print(type(y))
+# help(str)
+
+tim = turtle.Turtle()
+
+def	func(x): # function
+	return x + 1
+	
+print(func(5))
+
+print(y.upper()) # method
+
+print(y.replace('s', ''))
 
 
