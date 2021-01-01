545d6107ebc23f64ee83b985bf1f91142aa95f6a
Thomas Snellgrove <tsnellgrove@hotmail.com>
2021-01-01 10:19:01 -0500

    TwT - starting my own examples

--- techwtim/oop_tut.py
+++ techwtim/oop_tut.py
@@ -1,12 +1,60 @@
 # Tech with Tim
+
+
 # Python Classes & Objects Tutorial 2
 # Link: https://youtu.be/jQiUOV15IRI
-# Topic:
+# Topic: Creating your own ojbects & classes
+# 0:00 Classes & Objects
+# 9:45 Create new Attributes
+
+# Tom Example
+
+class Item(object):
+		def __init__(self, name, desc):
+				self.name = name
+				self.desc = desc
+		
+		def examine(self):
+				print(self.desc)
+		
+		def change_desc(self, new_desc):
+				self.desc = new_desc
+
+sword = Item('sword','The sword is shiny')
+sword.examine()
+sword.change_desc('The sword is rusty')
+sword.examine()
 
 
+# Tim Example
+
+class Dog(object):
+		def __init__(self, name, age): # auto occurs upon class object instantiation
+				# print("Nice, you made a dog!")
+				self.name = name # name is an attribute of class Dog
+				self.age = age
+				self.li = [1,3,4] # note that attributes do not have to be passed into an object - they can be defined within the init
+
+# notice the idiomatic use of 'self' as a default attribute - this is referencing the specific instance of the object
+
+		def speak(self): # this is a method - it looks just like a function but has to be called using the class object
+				print("Hi, I am ", self.name, "and I am ", self.age, " years old")
+
+		def change_age(self, age):
+				self.age = age
+
+# classes allow you to create an infinite number of objects with each having all the attributes of the class
 
 
-
+tim = Dog('Tim', 55) # tim is an instance of type / class Dog
+fred = Dog('Fred', 3) # fred is another instance of type dog
+tim.change_age(5)
+# tim.speak()
+# fred.speak()
+# print(tim.age) # note that we can access object attributes directly
+# print(tim.name)
+# print(tim.li)
+# print(fred.li)
 
 
 
@@ -32,10 +80,10 @@ tim = turtle.Turtle()
 def	func(x): # function
 	return x + 1
 	
-print(func(5))
+# print(func(5))
 
-print(y.upper()) # method
+# print(y.upper()) # method
 
-print(y.replace('s', ''))
+# print(y.replace('s', ''))
 
 
