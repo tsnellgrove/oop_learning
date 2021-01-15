1da3aada871f07cd7c05118e6ca5736be3654d83
Thomas Snellgrove <tsnellgrove@hotmail.com>
2021-01-15 07:39:38 -0500

    TwT OOP Tutorial 4

--- techwtim/oop_tut.py
+++ techwtim/oop_tut.py
@@ -2,11 +2,41 @@
 
 # Python Objects & Classes Tutorial 4
 # Link: https://youtu.be/39m3rstTN8w
-# 0:00 - 
+# 0:00 - 6:20 = create point math
 
 
+class Point():
+		def __init__(self, x=0, y=0):
+				self.x = x
+				self.y = y
+				self.coords = (self.x, self.y)
+				
+		def move(self, x, y):
+				self.x += x
+				self.y += y
 
+		def __add__(self, p):
+				return Point(self.x + p.x, self.y + p.y)
 
+		def __sub__(self, p):
+				return Point(self.x - p.x, self.y - p.y)
+
+		def __mul__(self, p):
+				return self.x * p.x + self.y * p.y
+
+		def __str__(self):
+				return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+p1 = Point(3, 4)
+p2 = Point(3, 2)
+p3 = Point(1, 3)
+p4 = Point(0, 1)
+
+p5 = p1 + p2 
+p6 = p4 - p1
+p7 = p2 * p3
+
+print(p5, p6, p7)
 
 
 
