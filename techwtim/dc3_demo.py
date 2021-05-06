8ad38c94f7c177d167ced694f6545bc5f45a2f62
Thomas Snellgrove <tsnellgrove@hotmail.com>
2021-05-06 07:26:52 -0400

    Comments cleaned up post helper module

--- techwtim/dc3_demo.py
+++ techwtim/dc3_demo.py
@@ -10,133 +10,16 @@ import cmd
 import sys
 from dc3_helper import objlst_to_strlst
 from dc3_helper import scope_check
-## from dc3_helper import open_cont_scan
 from dc3_helper import root_word_count
 from dc3_helper import buffer
 from dc3_helper import container_desc
 from dc3_helper import inventory
 from dc3_helper import end
-## from dc3_helper import str_to_class
-
-# NOT IN USE
-## def set_difference(a,b):
-##    return list(set(a)-set(b))
-
-## def change_desc(self, new_desc):
-##		self.desc = new_desc
 
 
 # helper functions
-## def buffer(stateful_dict, output_str):
-##		out_buff = stateful_dict['out_buff']
-##		out_buff = out_buff + output_str + "\n"
-##		stateful_dict['out_buff'] = out_buff
-
-## def open_cont_scan(stateful_dict, room_obj_lst):
-##		open_cont_obj_lst = []
-##		for obj in room_obj_lst:
-##				if hasattr(obj, 'contains') \
-##								and len(obj.contains) > 0 \
-##								and obj.open_state == True:
-##						open_cont_obj_lst = open_cont_obj_lst + obj.contains
-##		return open_cont_obj_lst
-
 def str_to_class(str):
 		return getattr(sys.modules[__name__], str)
-
-## def objlst_to_strlst(obj_lst):
-##		str_lst = []
-##		for obj in obj_lst:
-##				str_lst.append(obj.full_name)
-##		return str_lst
-
-## def scope_check(obj, stateful_dict):
-##		room_obj = stateful_dict['room']
-##		hand_lst = stateful_dict['hand']
-##		backpack_lst = stateful_dict['backpack']
-##		universal_lst = stateful_dict['universal']
-##		room_obj_lst = room_obj.room_stuff
-##		features_lst = room_obj.features
-##		open_cont_obj_lst = open_cont_scan(stateful_dict, room_obj_lst)
-##		scope_lst = (room_obj_lst + hand_lst + backpack_lst 
-##						+ universal_lst + features_lst + open_cont_obj_lst)
-##		scope_lst.append(room_obj)
-##		return obj in scope_lst
-
-## def root_word_count(stateful_dict, word2):
-##		room_obj = stateful_dict['room']
-##		hand_lst = stateful_dict['hand']
-##		backpack_lst = stateful_dict['backpack']
-##		universal_lst = stateful_dict['universal']
-##		room_obj_lst = room_obj.room_stuff
-##		features_lst = room_obj.features
-##		open_cont_obj_lst = open_cont_scan(stateful_dict, room_obj_lst)
-##		scope_lst = (room_obj_lst + hand_lst + backpack_lst 
-##						+ universal_lst + features_lst + open_cont_obj_lst)
-##		scope_lst.append(room_obj)
-
-##		root_count = 0
-##		obj_name = ""
-##		for obj in scope_lst:
-##				if obj.root_name == word2:
-##						root_count += 1
-##						obj_name = obj.name
-##				if obj.writing is not None:
-##						if obj.writing.root_name == word2:
-##								root_count += 1
-##								obj_name = obj.writing.name
-##		return root_count, obj_name
-
-## def container_desc(cont_obj, stateful_dict):
-##		if len(cont_obj.contains) == 0:
-##				buffer(stateful_dict, "The " + cont_obj.full_name + " is empty.")
-##		else:
-##				cont_str_lst = objlst_to_strlst(cont_obj.contains)
-##				output = "The " + cont_obj.full_name + " contains: "  + ', '.join(cont_str_lst)
-##				buffer(stateful_dict, output)
-
-## def inventory(stateful_dict):
-##		hand_obj_lst = stateful_dict['hand']
-##		backpack_str_lst = objlst_to_strlst(stateful_dict['backpack'])
-
-##		if len(hand_obj_lst) == 0:
-##				hand_str = "nothing"
-##		else:
-##				hand_str = "the " + stateful_dict['hand'][0].full_name
-##		buffer(stateful_dict, "In your hand you are holding " + hand_str)
-
-##		if len(backpack_str_lst) == 0:
-##				backpack_str = "nothing"
-##		else:
-##				backpack_str = ', '.join(backpack_str_lst)
-##		buffer(stateful_dict, "In your backpack you have: " + backpack_str)
-
-## def end(stateful_dict):
-##		score = stateful_dict['current_score']
-##		moves = stateful_dict['move_counter']
-##		game_ending = stateful_dict['game_ending']
-
-#		if score < 0:
-#				title_score = -10
-#		elif score == 0:
-#				title_score = 0
-#		else:
-#				title_score = math.ceil(score / 10) * 10
-#		title = static_dict['titles_dict'][title_score]
-
-##		if game_ending == 'death':
-##				buffer(stateful_dict, "You have died.")
-##		elif game_ending == 'quit':
-##				buffer(stateful_dict, "You have quit.")
-##		elif game_ending == 'won':
-##				buffer(stateful_dict, "You have won!")
-##		buffer(stateful_dict, "Your adventure ended after " + str(moves) + " moves.")
-#    print_score(state_dict, static_dict)
-#		buffer("Your title is: " + title)
-##		if game_ending == 'won':
-##				buffer(stateful_dict, credits.examine(stateful_dict))
-##		stateful_dict['end_of_game'] = True
-##		return
 
 
 # classes
@@ -522,21 +405,6 @@ def interpreter(stateful_dict, user_input):
 		except:
 				buffer(stateful_dict, "You can't " + word1 + " with the " + word2 + ".")
 				stateful_dict['move_counter'] = stateful_dict['move_counter'] - 1
-
-
-##		else:
-##				try:
-##						word2_obj = str_to_class(word2)
-##						try:
-##								getattr(word2_obj, word1)(stateful_dict)
-##						except:
-##								output = "You can't " + word1 + " with the " + word2 + "."
-##								buffer(stateful_dict, output)
-##								stateful_dict['move_counter'] = stateful_dict['move_counter'] - 1
-##				except:
-##						output = "There's no " + word2 + " here."
-##						buffer(stateful_dict, output)
-##						stateful_dict['move_counter'] = stateful_dict['move_counter'] - 1
 
 
 # test
