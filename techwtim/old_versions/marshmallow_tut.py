39e7260456f02d3fc0bc2827c649b3aa7e29c06d
Thomas Snellgrove <tsnellgrove@hotmail.com>
2021-06-29 21:41:46 -0400

    yet more marshmallow troubleshooting

--- techwtim/old_versions/marshmallow_tut.py
+++ techwtim/old_versions/marshmallow_tut.py
@@ -5,6 +5,14 @@
 
 from marshmallow import Schema, fields, post_load
 
+class Pet:
+		def __init__(self, pet_name, is_a_cat):
+				self.pet_name = pet_name
+				self.is_a_cat = is_a_cat
+
+		def __repr__(self):
+				return f'it is { self.is_a_cat } that { self.pet_name } is a cat'
+
 class Person:
 		def __init__(self, name, age):
 				self.name = name
@@ -18,12 +26,16 @@ class PetSchema(Schema):
 		pet_name = fields.String()
 		is_a_cat = fields.Boolean()
 
+# post load transform with marshmallow creates instance of Class
+		@post_load
+		def create_pet(self, data, **kwargs):
+				return Pet(**data)
+
 class PersonSchema(Schema):
 		name = fields.String()
 		age = fields.Integer()
 ###		include_fk = True
 		pet = fields.Nested(PetSchema)
-
 
 # post load transform with marshmallow creates instance of Class
 		@post_load
@@ -35,24 +47,30 @@ pet_data = {}
 input_data = {}
 person_data = {}
 
-pet_data['pet_name'] = input('What is your pet name? ')
-pet_data['is_a_cat'] = input('Is it True that your pet is a cat? ')
+## pet_data['pet_name'] = input('What is your pet name? ')
+## pet_data['is_a_cat'] = input('Is it True that your pet is a cat? ')
 
-input_data['name'] = input('What is your name? ')
-input_data['age'] = input('What is your age? ')
+## input_data['name'] = input('What is your name? ')
+## input_data['age'] = input('What is your age? ')
+
+pet_data['pet_name'] = "Kit"
+pet_data['is_a_cat'] = True
+
+input_data['name'] = "Tom"
+input_data['age'] = 50
 
 print(pet_data)
 schema_pet = PetSchema()
 pet1 = schema_pet.load(pet_data)
 print(pet1)
 
-person_data = [input_data, pet1]
+person_data = [input_data, pet_data]
 print(person_data)
 
 schema_person = PersonSchema(many=True)
 person3 = schema_person.load(person_data)
 
-### UserSchema().dump(users, many=True) [from marshmallow site]
+## UserSchema().dump(users, many=True) [from marshmallow site]
 
 # load input data into complex object schema
 ## schema = PersonSchema()
@@ -71,13 +89,16 @@ person3 = schema_person.load(person_data)
 
 # case3: serialization print1
 ## print(person2)
-print(person3)
+
+#### print(person3)
 
 # case3: serialization
 ## result2 = schema.dump(person2)
-result3 = schema_person.dump(person3)
+
+#### result3 = schema_person.dump(person3)
 
 # case3: serialization print of dictionary (input data has been validated)
 ##print(result2)
-print(result3)
+
+#### print(result3)
 
