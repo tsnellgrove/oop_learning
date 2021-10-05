06cf0cccb158c083201075b53d7ff0a45c6eb820
Thomas Snellgrove <tsnellgrove@hotmail.com>
2021-10-05 14:20:02 -0400

    remove full obj declarations from wapper() and just declare stateful_dict and active_gs

--- techwtim/dc3_wrapper.py
+++ techwtim/dc3_wrapper.py
@@ -38,7 +38,10 @@ def wrapper(user_input):
 						master_obj_lst = pickle.load(f)
 
 				# object vatiables declared / instantiated from un-pickled list
-				stateful_dict, active_gs, rusty_lettering, dwarven_runes, messy_handwriting, small_printing, illuminated_letters, calligraphy, trademark, dark_castle, moat, backpack, burt, fist, conscience, faded_tapestries, alcove, stone_coffer, family_tree, rusty_key, shiny_sword, brass_key, bubbly_potion, torn_note, grimy_axe, silver_key, kinging_scroll, cheese_wedge, stale_biscuits, fresh_water, wooden_chest, crystal_box, glass_bottle, front_gate, iron_portcullis, control_panel, throne, entrance, main_hall, antechamber, throne_room = master_obj_lst
+	#			stateful_dict, active_gs, rusty_lettering, dwarven_runes, messy_handwriting, small_printing, illuminated_letters, calligraphy, trademark, dark_castle, moat, backpack, burt, fist, conscience, faded_tapestries, alcove, stone_coffer, family_tree, rusty_key, shiny_sword, brass_key, bubbly_potion, torn_note, grimy_axe, silver_key, kinging_scroll, cheese_wedge, stale_biscuits, fresh_water, wooden_chest, crystal_box, glass_bottle, front_gate, iron_portcullis, control_panel, throne, entrance, main_hall, antechamber, throne_room = master_obj_lst
+
+				stateful_dict = master_obj_lst[0]
+				active_gs = master_obj_lst[1]
 
 				print("wrapper - post-pickle load")
 
@@ -70,8 +73,9 @@ def wrapper(user_input):
 				# post-action triggers will go here
 
 
-				master_obj_lst = [stateful_dict, active_gs, rusty_lettering, dwarven_runes, messy_handwriting, small_printing, illuminated_letters, calligraphy, trademark, dark_castle, moat, backpack, burt, fist, conscience, faded_tapestries, alcove, stone_coffer, family_tree, rusty_key, shiny_sword, brass_key, bubbly_potion, torn_note, grimy_axe, silver_key, kinging_scroll, cheese_wedge, stale_biscuits, fresh_water, wooden_chest, crystal_box, glass_bottle, front_gate, iron_portcullis, control_panel, throne, entrance, main_hall, antechamber, throne_room]
+#				master_obj_lst = [stateful_dict, active_gs, rusty_lettering, dwarven_runes, messy_handwriting, small_printing, illuminated_letters, calligraphy, trademark, dark_castle, moat, backpack, burt, fist, conscience, faded_tapestries, alcove, stone_coffer, family_tree, rusty_key, shiny_sword, brass_key, bubbly_potion, torn_note, grimy_axe, silver_key, kinging_scroll, cheese_wedge, stale_biscuits, fresh_water, wooden_chest, crystal_box, glass_bottle, front_gate, iron_portcullis, control_panel, throne, entrance, main_hall, antechamber, throne_room]
 
+				### dump updated objects to save_obj_pickle2 ###
 				with open('save_obj_pickle2', 'wb') as f:
 						pickle.dump(master_obj_lst, f) # Why are list elements updated? But works!
 		
